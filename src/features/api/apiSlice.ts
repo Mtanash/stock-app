4fd55762dbaad9e6de902725be95d094525bf555
Mohamed Tanash <tanashkoo@gmail.com>
2022-09-21 23:45:34 +0200

    add api endpoints url

--- src/features/api/apiSlice.ts
+++ src/features/api/apiSlice.ts
@@ -2,8 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({
-    // baseUrl: "https://stock-api-oooe.onrender.com",
-    baseUrl: "http://localhost:5000",
+    baseUrl: "https://stock-api-oooe.onrender.com",
   }),
   tagTypes: ["stocks", "items"],
   endpoints: () => ({}),
