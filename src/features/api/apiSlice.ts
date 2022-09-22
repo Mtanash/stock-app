2acb8defe07eae6ae4f75b3dec9dcc9003aa8920
Mohamed Tanash <tanashkoo@gmail.com>
2022-09-22 11:03:24 +0200

    fix request credentials bug

--- src/features/api/apiSlice.ts
+++ src/features/api/apiSlice.ts
@@ -3,6 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: "https://mtanash-stock-api.herokuapp.com",
+    credentials: "include",
   }),
   tagTypes: ["stocks", "items"],
   endpoints: () => ({}),
