afdb9eb047a5b6dda127b74121e2cf4b4241ba17
Mohamed Tanash <tanashkoo@gmail.com>
2022-09-22 11:57:51 +0200

    remove cors wildcard

--- src/features/api/apiSlice.ts
+++ src/features/api/apiSlice.ts
@@ -4,10 +4,6 @@ export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: "https://mtanash-stock-api.herokuapp.com",
     credentials: "include",
-    prepareHeaders: (headers, { getState }) => {
-      headers.set("Access-Control-Allow-Origin", "*");
-      return headers;
-    },
   }),
   tagTypes: ["stocks", "items"],
   endpoints: () => ({}),
